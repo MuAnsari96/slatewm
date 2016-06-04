e090116c98c2114d7d184d07459f77ecc5682128
Mustafa Ansari <maa296@cornell.edu>
2016-06-04 00:03:54 -0400

    Spend hours trying to figure out an elegant design.
    Go back and forth, literally for days. Then choose the worst idea you could possibly imagine and run with that. Yep. Sounds about right.

--- src/slate.cpp
+++ src/slate.cpp
@@ -1,8 +1,12 @@
 #include <iostream>
 #include "slate.h"
-#include "event_handler.h"
 
-Slate::Slate() {
+#define ALT 64
+#define CTL 37
+#define MOD 133
+
+Slate::Slate() :
+    ctx(1), toclient(ctx, ZMQ_PUSH), fromclient(ctx, ZMQ_PULL) {
     Display* display_ = XOpenDisplay(nullptr);
 
     if (!display_) {
@@ -11,29 +15,58 @@ Slate::Slate() {
     }
     display = display_;
     root = DefaultRootWindow(display_);
+
+    //toclient.bind("ipc:///tmp/feeds/0");
+    //fromclient.bind("ipc:///tmp/feeds/1");
 }
 
 Slate::~Slate() {
     XCloseDisplay(display);
-    delete display;
 }
 
-static std::unique_ptr<Slate> Slate::getInstance() {
+std::unique_ptr<Slate> Slate::getInstance() {
     return std::unique_ptr<Slate> (new Slate);
 }
 
 void Slate::run() {
-    using namespace XEventHandler;
+    XSelectInput(display, root, KeyReleaseMask | KeyPressMask | SubstructureNotifyMask);
+    // ALT, CTL, MOD
+    bool mods[3] = {false, false, false};
     while (true) {
         XEvent e;
         XNextEvent(display, &e);
 
         switch (e.type) {
+            case KeyPress:
+                if (e.xkey.keycode == ALT) {
+                    mods[0] = true;
+                }
+                if (e.xkey.keycode == CTL) {
+                    mods[1] = true;
+                }
+                if (e.xkey.keycode == MOD) {
+                    mods[2] = true;
+                }
+                break;
+            case KeyRelease:
+                if (e.xkey.keycode == ALT) {
+                    mods[0] = true;
+                }
+                if (e.xkey.keycode == CTL) {
+                    mods[1] = true;
+                }
+                if (e.xkey.keycode == MOD) {
+                    mods[2] = true;
+                }
+                break;
             case CreateNotify:
-                // call stuff from XEventHandler, for example
+                XSetWindowAttributes attr;
+                attr.do_not_propagate_mask = 0;
+                attr.event_mask = KeyReleaseMask | KeyPressMask | SubstructureNotifyMask;
+                XChangeWindowAttributes(display, e.xcreatewindow.window, CWDontPropagate | CWEventMask, &attr);
                 break;
             default:
-                return;
+                break;
         }
     }
 }
--- src/slate.h
+++ src/slate.h
@@ -3,11 +3,15 @@
 
 #include <X11/Xlib.h>
 #include <memory>
+#include "zmq.hpp"
 
 class Slate {
 private:
     Display* display;
     Window root;
+    zmq::context_t ctx;
+    zmq::socket_t toclient;
+    zmq::socket_t fromclient;
     Slate();
 
 public:
