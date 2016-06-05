8d0323ec265f12e353f27bd3e51f70e6302dbc92
Mustafa Ansari <maa296@cornell.edu>
2016-06-05 01:33:47 -0400

    Fix convention on nullbytes

--- src/client_handler.cpp
+++ src/client_handler.cpp
@@ -5,7 +5,9 @@ void ClientHandler::Run(Slate* wm) {
     zmq::message_t reply;
     while (true) {
         wm->fromclient.recv(&reply);
-        std::cout << std::string(static_cast<char*>(reply.data())) << std::endl;
+        std::string msg(static_cast<char*>(reply.data()), reply.size()+1);
+        msg[reply.size()] = '\0';
+        std::cout << msg << std::endl;
     }
 }
 
